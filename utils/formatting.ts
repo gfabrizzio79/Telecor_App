401ec9f0d28c5e4e3fbbfd441abba7ed2c43544d
gfabrizzio79 <gfabrizzio@gmail.com>
2025-08-20 08:25:26 -0600

    Delete utils directory

--- utils/formatting.ts
+++
@@ -1,7 +0,0 @@
-
-export const formatCurrency = (value: number | undefined | null): string => {
-  if (value === null || value === undefined) {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(0);
-  }
-  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value);
-};
